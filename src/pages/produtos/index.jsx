91546e05cbcaecf243d044d838f5030aaa3bcf1d
ArissattiOproprio <joaoarissatti09@gmail.com>
2025-09-19 08:18:38 -0300

    lista

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,7 +1,14 @@
 const Produtos = () => {
+    // Objeto com a lista de pizzas
+    const pizzas = [
+        'Pizza de Muçarela',
+        'Pizza de Calabresa',
+        'Pizza Baiana', 'Pizza Quatro Queijos'
+    ]
+    // Iteração da lista de pizzas
+    const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
 
     return (
-<<<<<<< HEAD
         <><div>
             <><ul>
                 <li>Pizza de Muçarela</li>
@@ -10,16 +17,6 @@ const Produtos = () => {
                 <li>Pizza Baiana</li>
                 </ul></>
             </div></>
-=======
-        <div>
-            <ul>
-                <li>Pizza de Muçarela</li>
-                <li>Pizza de Calabreza</li>
-                <li>Pizza Portuguesa</li>
-                <li>Pizza Baiana</li>
-            </ul>
-        </div>
->>>>>>> 7df326775cdef100b9d04d566a14cd66ac71dd99
     )
 }
 
